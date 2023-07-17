23ed1b589dc84518478794c7ff84579d8a8f6c35
Sazzad <sazzad47.ju@gmail.com>
2023-07-17 16:15:52 +0600

    make it better

--- products/models.py
+++ products/models.py
@@ -19,9 +19,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=100, blank=True, null=True)
     photo = models.CharField(max_length=200, blank=True, null=True)
-    rating = models.IntegerField(choices=RATING_CHOICES, blank=True, null=True)
-    starting_quantity = models.IntegerField(choices=RATING_CHOICES, blank=True, null=True)
-    starting_price = models.IntegerField(choices=RATING_CHOICES, blank=True, null=True)
+    rating = models.CharField(max_length=100, blank=True, null=True)
+    starting_quantity = models.CharField(max_length=100, blank=True, null=True)
+    starting_price = models.CharField(max_length=100, blank=True, null=True)
     options = models.JSONField(blank=True, null=True)
     cover = models.CharField(max_length=200, blank=True, null=True)
     information = models.CharField(max_length=3000, blank=True, null=True)
@@ -31,8 +31,8 @@ class Product(models.Model):
     design_services = models.JSONField(blank=True, null=True)
     features = models.JSONField(blank=True, null=True)
     variants = models.JSONField(blank=True, null=True)
-    rp = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    dp = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    rp = models.CharField(max_length=100, blank=True, null=True)
+    dp = models.CharField(max_length=100, blank=True, null=True)
     price = models.JSONField(blank=True, null=True)
 
     def __str__(self):
